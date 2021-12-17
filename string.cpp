733a5fcb254c7433246bd1d6643da6be057a645f
Satvik77 <83899207+Satvik77@users.noreply.github.com>
2021-12-17 16:05:33 +0530

    Add files via upload

---
+++ string.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string str;
+    cout<<"Enter your string:"<<endl;
+    getline(cin,str);
+    cout<<"your string is:"<<str<<endl;
+    return 0;
+}
